96945d7f4093d58a2241667a278d62ee38f74586
steve <f23934073@gmail.com>
2024-03-05 14:14:41 +0800

    add upload file

---
+++ internal/routers/api/v1/upload.go
@@ -0,0 +1,46 @@
+package v1
+
+import (
+	"blog-service/global"
+	"blog-service/internal/service"
+	"blog-service/pkg/app"
+	"blog-service/pkg/convert"
+	"blog-service/pkg/errcode"
+	"blog-service/pkg/upload"
+	"github.com/gin-gonic/gin"
+)
+
+type Upload struct{}
+
+func NewUpload() Upload {
+	return Upload{}
+}
+
+func (u Upload) UploadFile(c *gin.Context) {
+	response := app.NewResponse(c)
+	file, fileHeader, err := c.Request.FormFile("file")
+	fileType := convert.StrTo(c.PostForm("type")).MustInt()
+	if err != nil {
+		errRsp := errcode.InvalidParams.WithDetails(err.Error())
+		response.ToErrorResponse(errRsp)
+		return
+	}
+
+	if fileHeader == nil || fileType <= 0 {
+		response.ToErrorResponse(errcode.InvalidParams)
+		return
+	}
+
+	svc := service.NewService(c.Request.Context())
+	fileInfo, err := svc.UploadFile(upload.FileType(fileType), file, fileHeader)
+	if err != nil {
+		global.Logger.Errorf("svc.UploadFile err: %v", err)
+		response.ToErrorResponse(errcode.ErrorUploadFileFail.WithDetails(err.Error()))
+		return
+	}
+
+	response.ToResponse(gin.H{
+		"file_access_url": fileInfo.AccessUrl,
+	})
+	return
+}
--- pkg/errcode/common_code.go
+++ pkg/errcode/common_code.go
@@ -11,3 +11,17 @@ var (
 	UnauthorizedTokenGenerate = NewError(10000006, "驗證失敗，Token生成失敗")
 	TooManyRequests           = NewError(10000007, "請求過多")
 )
+
+// 2001xxx 標籤相關錯誤
+var (
+	ErrorGetTagListFail = NewError(20010001, "獲取標籤列表失敗")
+	ErrorCreateTagFail  = NewError(20010002, "創建標籤失敗")
+	ErrorUpdateTagFail  = NewError(20010003, "更新標籤失敗")
+	ErrorDeleteTagFail  = NewError(20010004, "刪除標籤失敗")
+	ErrorCountTagFail   = NewError(20010005, "統計標籤失敗")
+)
+
+// 2003xxx 上傳文件相關錯誤
+var (
+	ErrorUploadFileFail = NewError(20030001, "上傳文件失敗")
+)
