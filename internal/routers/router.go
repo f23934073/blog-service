bd54f74fb5a52c597df85ffe7c6dc46a0bce7395
steve <f23934073@gmail.com>
2024-03-21 17:17:22 +0800

    add jwt middleware

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -34,6 +34,7 @@ func NewRouters() *gin.Engine {
 	tag := v1.NewTag()
 
 	apiv1 := r.Group("api/v1")
+	apiv1.Use(middleware.JWT())
 	{
 		apiv1.POST("/tags", tag.Create)
 		apiv1.DELETE("/tags/:id", tag.Delete)
