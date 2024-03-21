e3c95206e60fa5fed8466170206196cff4a6d5d5
steve <f23934073@gmail.com>
2024-03-21 17:23:00 +0800

    debug

--- internal/service/tag.go
+++ internal/service/tag.go
@@ -12,7 +12,7 @@ type CountTagRequest struct {
 
 type TagListRequest struct {
 	Name  string `form:"name" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=01"`
+	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
 }
 
 type CreateTagRequest struct {
